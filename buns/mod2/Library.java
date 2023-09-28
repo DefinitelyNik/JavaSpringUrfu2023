022d067a9c1c12c7cfa88d64938e1695054ea27e
Nik <45146898+DefinitelyNik@users.noreply.github.com>
2023-09-28 22:54:52 +0500

    lesson 2: @Before advice type

---
+++ buns/mod2/Library.java
@@ -0,0 +1,10 @@
+package mod2;
+
+import org.springframework.stereotype.Component;
+
+@Component("libraryBean")
+public class Library {
+    public void getBook() {
+        System.out.println("Мы берем книгу");
+    }
+}
