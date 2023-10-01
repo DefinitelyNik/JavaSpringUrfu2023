ca2a74c71758db82c147c7ef0673005f4980c2b2
Nik <45146898+DefinitelyNik@users.noreply.github.com>
2023-10-01 20:51:25 +0500

    lesson 4: pointcut expression 2nd part

--- buns/mod2/UniLibrary.java
+++ buns/mod2/UniLibrary.java
@@ -4,15 +4,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class UniLibrary extends AbstractLibrary{
-    @Override
-    public void getBook() {
-        System.out.println("Мы берем книгу из UniLibrary");
+
+    public void getBook(Book book) {
+        System.out.println("Мы берем книгу \"" + book.getName() + "\" из UniLibrary");
     }
 
-    public String returnBook() {
-        System.out.println("Мы возвращаем книгу в UniLibrary");
-        return "ok";
-    }
+//    public String returnBook() {
+//        System.out.println("Мы возвращаем книгу в UniLibrary");
+//        return "ok";
+//    }
 
     public void getMagazine() {
         System.out.println("Мы берем журнал из UniLibrary");
